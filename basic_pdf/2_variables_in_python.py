2c82389dada381c4f0cc97604e68e7065bec4cd0
= <->
2023-02-15 13:49:24 +0500

    adding code file

--- basic_pdf/2_variables_in_python.py
+++ basic_pdf/2_variables_in_python.py
@@ -8,25 +8,11 @@
 # behaviour related to that data in methods. 
 # we can access attribute and methods related to object using dot opearator. let see an example,
 # 
-
-# In[1]:
-
-
 x = 45.4
 x.is_integer()
 
 
 # # Defining variable in python:
-
-# In[2]:
-
-
-#we can't declare a variable without assigning it a value in python  as the following
-#command will generate error
-t
-
-
-# In[3]:
 
 
 #we can declare a variable without assigning it any value as,
@@ -38,7 +24,6 @@ print(value)
 # the absence of value, there are different scanerios when value can be None, for example
 # when function returns nothing, or nothing passed in argument etc. 
 
-# In[4]:
 
 
 #we can assign value to a variable initialized with None as,
@@ -48,31 +33,26 @@ print(value)
 
 # # Casting one type to another type
 
-# In[5]:
-
-
-#we can convert one type into another type using different built-in appropriate function for example,
+# one type into another type using different built-in appropriate function for example,
 #convert string to int
 val1 = int("99")
+
 #coverting int to string
+
 val2 = str(45)
+
 #convert float to int
+
 val3 = int(45.5)
 print(val1)
 print(val2)
 print(val3)
 
 
-# In[6]:
-
-
 #lets check type of above variables using type() function
 print(type(val1))
 print(type(val2))
 print(type(val3))
-
-
-# In[8]:
 
 
 #String variables can be defined using single and double quotes as well,
@@ -85,23 +65,17 @@ print(fname + lname)
 
 # # Python is case sensitive
 
-# In[9]:
-
-
 #Case Sensitive languages are ones which differ in lower case and upper case as the letter a is not
 #same as capital A. python is case sensitive.
 a = 34;
 A = 56;
 print(a)
 print(A)
+
 #a and A are two different variables for python
 
 
 # # Rules for variable names in python 
-
-# In[10]:
-
-
 #1. variable name can contain only letter (lowercase + uppercase) digits (0-9) and underscore
 #2. name can start with letters or underscore but can't start with a digit
 
@@ -110,8 +84,6 @@ print(A)
 
 # Any variable which is created in program scope not in a function, it is known as global variable and can be access
 # anywhere in program at any time during the lifetime of program.
-
-# In[13]:
 
 
 x = 45
@@ -127,15 +99,10 @@ print("Python is " + str(x))
 # In above code the variable defined inside a function is local variable and can be accessed only inside a function
 # and the variable defined outside function is global and it can be accessed any where inside function or out of function scope. 
 
-# In[15]:
-
 
 #to create a global variable inside a function use global keyword with variable as,
 def myfunc():
   global x
-
-
-# In[ ]:
 
 
 
